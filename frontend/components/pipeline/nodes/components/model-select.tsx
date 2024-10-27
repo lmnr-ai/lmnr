3ea5f0a67d1e8596ad1b5b62095bba43eb751169
Dinmukhamed Mailibay <47117969+dinmukhamedm@users.noreply.github.com>
2024-10-27 00:16:11 -0700

    extend span path to have proper path on manual instruments (#98)

--- frontend/components/pipeline/nodes/components/model-select.tsx
+++ frontend/components/pipeline/nodes/components/model-select.tsx
@@ -49,7 +49,8 @@ export default function LanguageModelSelect({
     setModel(LANGUAGE_MODELS.find((model) => model.id === modelId));
   }, [modelId]);
 
-  const isProviderKeySet = (provider: string) => providerApiKeys?.some(key => key.name.toLowerCase().includes(provider.toLowerCase()));
+  const isProviderKeySet = (provider: string) => providerApiKeys
+    ?.some(key => key.name.toLowerCase().includes(provider.toLowerCase()));
 
   return (
     <>
