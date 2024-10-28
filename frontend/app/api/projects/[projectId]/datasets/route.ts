0845ee339b49c8bacdc94137212578b468a28afc
skull8888888 <skull8888888@gmail.com>
2024-10-27 20:18:24 -0700

    added delete route for datasets (#102)

--- frontend/app/api/projects/[projectId]/datasets/route.ts
+++ frontend/app/api/projects/[projectId]/datasets/route.ts
@@ -1,6 +1,11 @@
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { fetcher } from '@/lib/utils';
+import { datasets } from '@/lib/db/schema';
+import { isCurrentUserMemberOfProject } from '@/lib/db/utils';
+import { eq, inArray } from 'drizzle-orm';
+import { and } from 'drizzle-orm';
+import { db } from '@/lib/db/drizzle';
 
 export async function POST(
   req: Request,
@@ -41,4 +46,38 @@ export async function GET(
   });
 
   return new Response(res.body);
+}
+
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { projectId: string; datasetId: string } }
+): Promise<Response> {
+  const projectId = params.projectId;
+
+  if (!(await isCurrentUserMemberOfProject(projectId))) {
+    return new Response(JSON.stringify({ error: "User is not a member of the project" }), { status: 403 });
+  }
+
+  const { searchParams } = new URL(req.url);
+  const datasetIds = searchParams.get('datasetIds')?.split(',');
+
+  if (!datasetIds) {
+    return new Response('At least one Dataset ID is required', { status: 400 });
+  }
+
+  try {
+    await db.delete(datasets)
+      .where(
+        and(
+          inArray(datasets.id, datasetIds),
+          eq(datasets.projectId, projectId)
+        )
+      );
+
+    return new Response('datasets deleted successfully', { status: 200 });
+  } catch (error) {
+    console.error('Error deleting datasets:', error);
+    return new Response('Error deleting datasets', { status: 500 });
+  }
 }
