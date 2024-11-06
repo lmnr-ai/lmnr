a19833d6bfd03a7ef052f2bcd2bd0b7e163156d5
Din <dinmukhamed.mailibay@gmail.com>
2024-11-05 23:07:40 -0800

    remove feature check from clickhouse client

--- frontend/lib/clickhouse/client.ts
+++ frontend/lib/clickhouse/client.ts
@@ -1,20 +1,17 @@
 import { createClient } from "@clickhouse/client";
-import { Feature, isFeatureEnabled } from "../features/features";
 import { config } from "dotenv";
 
 config({ path: ".env" }); // or .env.local
 
 // https://clickhouse.com/docs/en/cloud/bestpractices/asynchronous-inserts -> Create client which will wait for async inserts
 // For now, we're not waiting for inserts to finish, but later need to add queue and batch on client-side
-export const clickhouseClient = isFeatureEnabled(Feature.FULL_BUILD)
-  ? createClient({
-    url: process.env.CLICKHOUSE_URL,
-    username: process.env.CLICKHOUSE_USER,
-    password: process.env.CLICKHOUSE_PASSWORD ?? "",
-    database: "default",
-    clickhouse_settings: {
-      async_insert: 1,
-      wait_for_async_insert: 0,
-    },
-  })
-  : createClient();
+export const clickhouseClient = createClient({
+  url: process.env.CLICKHOUSE_URL,
+  username: process.env.CLICKHOUSE_USER,
+  password: process.env.CLICKHOUSE_PASSWORD ?? "",
+  database: "default",
+  clickhouse_settings: {
+    async_insert: 1,
+    wait_for_async_insert: 0,
+  },
+});
