6e4cc4e6d31adca38eeb481a949d17e32336edca
Dinmukhamed Mailibay <47117969+dinmukhamedm@users.noreply.github.com>
2024-11-05 23:11:50 -0800

    insert human eval scores to clickhouse (#163)
    * insert human eval scores to clickhouse
    
    * don't setup env vars

--- frontend/app/api/projects/[projectId]/queues/[queueId]/remove/route.ts
+++ frontend/app/api/projects/[projectId]/queues/[queueId]/remove/route.ts
@@ -1,7 +1,10 @@
 import { isCurrentUserMemberOfProject } from '@/lib/db/utils';
 import { db } from '@/lib/db/drizzle';
-import { labelingQueueItems, labels, evaluationScores, labelingQueues } from '@/lib/db/migrations/schema';
+import { labelingQueueItems, labels, evaluationScores, labelingQueues, evaluations, evaluationResults } from '@/lib/db/migrations/schema';
 import { eq, and } from 'drizzle-orm';
+import { isFeatureEnabled } from '@/lib/features/features';
+import { Feature } from '@/lib/features/features';
+import { clickhouseClient } from '@/lib/clickhouse/client';
 
 
 // remove an item from the queue
@@ -14,12 +17,13 @@ export async function POST(request: Request, { params }: { params: { projectId: 
   const { id, spanId, action } = body;
 
   if (action.resultId) {
-
     const labelingQueue = await db.query.labelingQueues.findFirst({
       where: eq(labelingQueues.id, params.queueId)
     });
 
     // get all labels of the span
+    // FIXME: this takes values from previous labels,
+    // potentially from a different queue
     const labelsOfSpan = await db.query.labels.findMany({
       where: eq(labels.spanId, spanId),
       with: {
@@ -35,6 +39,30 @@ export async function POST(request: Request, { params }: { params: { projectId: 
       name: `${label.labelClass.name}_${labelingQueue?.name}`,
       resultId,
     }));
+
+    if (isFeatureEnabled(Feature.FULL_BUILD)) {
+      const evaluation = await db.query.evaluations.findFirst({
+        with: {
+          evaluationResults: {
+            where: eq(evaluationResults.id, resultId)
+          }
+        }
+      });
+      if (evaluation && evaluationValues.length > 0) {
+        const result = await clickhouseClient.insert({
+          table: 'evaluation_scores',
+          format: 'JSONEachRow',
+          values: evaluationValues.map(value => ({
+            project_id: params.projectId,
+            group_id: evaluation.groupId,
+            evaluation_id: evaluation.id,
+            result_id: resultId,
+            name: value.name,
+            value: value.score,
+          }))
+        });
+      }
+    }
 
     await db.insert(evaluationScores).values(evaluationValues);
   }
