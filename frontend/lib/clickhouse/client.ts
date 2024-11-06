76e7a9834a22c73ff08640d61c1b5a41bbf7158b
Din <dinmukhamed.mailibay@gmail.com>
2024-11-05 21:38:00 -0800

    insert human eval scores to clickhouse

---
+++ frontend/lib/clickhouse/client.ts
@@ -0,0 +1,20 @@
+import { createClient } from "@clickhouse/client";
+import { Feature, isFeatureEnabled } from "../features/features";
+import { config } from "dotenv";
+
+config({ path: ".env" }); // or .env.local
+
+// https://clickhouse.com/docs/en/cloud/bestpractices/asynchronous-inserts -> Create client which will wait for async inserts
+// For now, we're not waiting for inserts to finish, but later need to add queue and batch on client-side
+export const clickhouseClient = isFeatureEnabled(Feature.FULL_BUILD)
+  ? createClient({
+    url: process.env.CLICKHOUSE_URL,
+    username: process.env.CLICKHOUSE_USER,
+    password: process.env.CLICKHOUSE_PASSWORD ?? "",
+    database: "default",
+    clickhouse_settings: {
+      async_insert: 1,
+      wait_for_async_insert: 0,
+    },
+  })
+  : createClient();
