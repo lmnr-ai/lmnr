1bed2738cca2c0ce02300e891eff09af6191f19d
Dinmukhamed Mailibay <47117969+dinmukhamedm@users.noreply.github.com>
2024-11-11 14:20:42 -0800

    Fix/span type filter (#195)
    * fix span type filter in traces view
    
    * remove unused pipeline templates group

--- frontend/components/pipelines/template-select.tsx
+++ frontend/components/pipelines/template-select.tsx
@@ -18,36 +18,11 @@ export default function TemplateSelect({
 }: TemplateSelectProps) {
   const buildTemplates = templates.filter((t) => t.displayGroup === 'build');
   const evalTemplates = templates.filter((t) => t.displayGroup === 'eval');
-  const semanticEventEvalTemplates = templates.filter(
-    (t) => t.displayGroup === 'semantic_event_eval'
-  );
   return (
     <div className={cn('flex flex-col space-y-4', className ?? '')}>
       <Label>Build</Label>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2">
         {buildTemplates.map((t) => (
-          <Card
-            className={cn(
-              'hover:bg-secondary p-1',
-              t.id === templateId ? 'bg-secondary' : ''
-            )}
-            key={t.id}
-            onClick={() => setTemplateId(t.id)}
-          >
-            <div className="p-4 space-y-1 cursor-pointer">
-              <h4 className="cursor-pointer font-semibold truncate max-w-50">
-                {' '}
-                {t.name}{' '}
-              </h4>
-              <p className="text-gray-600 text-[12px]">{t.description}</p>
-            </div>
-          </Card>
-        ))}
-      </div>
-
-      <Label>Semantic Event Evaluation</Label>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-2">
-        {semanticEventEvalTemplates.map((t) => (
           <Card
             className={cn(
               'hover:bg-secondary p-1',
