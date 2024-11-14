612c75423d39b726fceca292077166857bc569f6
skull8888888 <skull8888888@gmail.com>
2024-11-13 20:29:39 -0800

    fixed middleware path (#203)

--- frontend/app/api/projects/route.ts
+++ frontend/app/api/projects/route.ts
@@ -2,12 +2,17 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import { type NextRequest } from 'next/server';
 import { fetcher } from '@/lib/utils';
+import { isCurrentUserMemberOfWorkspace } from '@/lib/db/utils';
 
 export async function POST(req: NextRequest): Promise<Response> {
   const session = await getServerSession(authOptions);
   const user = session!.user;
 
   const body = await req.json();
+
+  if (!isCurrentUserMemberOfWorkspace(body.workspaceId)) {
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+  }
 
   return await fetcher(`/projects`, {
     method: 'POST',
