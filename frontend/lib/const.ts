a9036c1df6498291416b81cc6eb11162fdddd98a
skull8888888 <skull8888888@gmail.com>
2024-11-21 20:27:25 -0800

    updated docs (#234)

--- frontend/lib/const.ts
+++ frontend/lib/const.ts
@@ -2,5 +2,5 @@ export const SUPABASE_URL = 'https://kqwbtwycngtmqtxwudlx.supabase.co';
 export const SUPABASE_ANON_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtxd2J0d3ljbmd0bXF0eHd1ZGx4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MjcwNDksImV4cCI6MjAzMjIwMzA0OX0.HONeW9Fgy-_whUwGtPYbobmbcKijx650jTrBpuB_E9A';
 
-export const PYTHON_INSTALL = `pip install lmnr`;
+export const PYTHON_INSTALL = `pip install 'lmnr[all]'`;
 export const TYPESCRIPT_INSTALL = `npm add @lmnr-ai/lmnr`;
