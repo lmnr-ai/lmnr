670775b1eda2a007ff70e5b31c30a3d75dfe6e1f
Din <dinmukhamed.mailibay@gmail.com>
2024-11-24 11:22:32 -0800

    address ellipsis feedback on useEffect deps

--- frontend/components/evaluations/evaluations-groups-bar.tsx
+++ frontend/components/evaluations/evaluations-groups-bar.tsx
@@ -18,11 +18,13 @@ export default function EvaluationsGroupsBar() {
     swrFetcher,
   );
 
+  const groupId = searchParams.get('groupId');
+
   useEffect(() => {
-    if (groups && groups.length > 0 && !searchParams.get('groupId')) {
+    if (groups && groups.length > 0 && !groupId) {
       router.replace(`/project/${projectId}/evaluations?groupId=${groups[0].groupId}`);
     }
-  }, [groups, searchParams, router, projectId]);
+  }, [groups, groupId, router, projectId]);
 
   const columns: ColumnDef<{ groupId: string, lastEvaluationCreatedAt: string }>[] = [
     {
@@ -36,15 +38,13 @@ export default function EvaluationsGroupsBar() {
     },
   ];
 
-  const selectedGroupId = searchParams.get('groupId');
-
   return <div className="flex-none w-80 border-r flex flex-col">
     <div className="font-medium p-2 px-4 text-lg">Groups</div>
     <DataTable
       columns={columns}
       data={groups}
       getRowId={(row) => row.groupId}
-      focusedRowId={selectedGroupId}
+      focusedRowId={groupId}
       onRowClick={(row) => {
         router.push(`/project/${projectId}/evaluations?groupId=${row.original.groupId}`);
       }}
