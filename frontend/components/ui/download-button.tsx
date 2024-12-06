b2aaead558fa720912f3cd5d2a23fbe0f4c7a774
Devansh <devanshmahant12092003@gmail.com>
2024-12-06 15:41:01 +0530

    reducing unnecessery css

--- frontend/components/ui/download-button.tsx
+++ frontend/components/ui/download-button.tsx
@@ -67,21 +67,18 @@ export default function DownloadButton({
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className={cn(
-        "flex h-7 w-full items-center justify-between whitespace-nowrap rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-        "flex flex-none font-medium max-w-40 text-secondary-foreground"
-      )}>
+      <DropdownMenuTrigger className="flex h-7 w-full items-center justify-between rounded-md border bg-transparent px-3 py-2 text-sm focus:outline-none">
         <span>Download</span>
         <CaretSortIcon className="h-4 w-4 opacity-50" />
       </DropdownMenuTrigger>
       <DropdownMenuContent
         align="end"
-        className="relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2"
+        className="relative z-50 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md"
       >
         {['csv', 'json'].map((format) => (
           <DropdownMenuItem
             key={format}
-            className="relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50"
+            className="flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground"
             onClick={() => {
               console.log(uri + `/${format}`);
               downloadFile(uri + `/${format}`, format, filenameFallback + `.${format}`);
