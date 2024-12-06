ec063f55655bb69416f992a9a121dbdbc03776ed
skull8888888 <skull8888888@gmail.com>
2024-12-06 11:05:24 -0800

    Blog (#271)
    * online evals blog
    
    * updated
    
    * updated

--- frontend/app/blog/[slug]/page.tsx
+++ frontend/app/blog/[slug]/page.tsx
@@ -40,14 +40,9 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
   return (
     <>
       <LandingHeader hasSession={session !== null && session !== undefined} />
-      <div className="mt-32 h-full flex justify-center">
-        {/* <div className="w-1/4 flex justify-end">
-          <Link href="/blog" className="text-secondary-foreground hover:text-primary h-0">Back to all posts</Link>
-        </div> */}
-        <article className="flex flex-col z-30 py-16 md:w-[1000px] w-full px-8 md:px-0">
-          {/* <ScrollArea className="h-full flex-grow w-full mx-auto bg-background px-16">
-            <div className="h-0"> */}
-          <BlogMeta data={data} />
+      <div className="mt-48 h-full flex justify-center flex-col items-center">
+        <BlogMeta data={data} />
+        <article className="flex flex-col z-30 md:w-[700px] w-full px-8 md:px-0">
           <div className="pt-4 pb-48">
             <MDXRemote
               source={content}
@@ -56,26 +51,21 @@ export default async function BlogPostPage({ params }: { params: { slug: string 
                 h2: (props) => <MDHeading props={props} level={1} />,
                 h3: (props) => <MDHeading props={props} level={2} />,
                 h4: (props) => <MDHeading props={props} level={3} />,
-                p: (props) => <p className="py-2 text-secondary-foreground" {...props} />,
-                a: (props) => <a className="text-primary underline" target="_blank" rel="noopener noreferrer" {...props} />,
+                p: (props) => <p className="py-2 text-white/85" {...props} />,
+                a: (props) => <a className="text-white underline" target="_blank" rel="noopener noreferrer" {...props} />,
                 blockquote: (props) => <blockquote className="border-l-2 border-primary pl-4 py-2" {...props} />,
                 // codeblock
                 pre: (props) => <PreHighlighter className="pl-4 py-4" {...props} />,
                 // inline code
-                code: (props) => <span className="text-sm bg-secondary text-primary font-mono px-0.5" {...props} />,
-                ul: (props) => <ul className="list-disc pl-4 text-secondary-foreground" {...props} />,
-                ol: (props) => <ol className="list-decimal pl-4 text-secondary-foreground" {...props} />,
-                img: (props) => <img className="w-full border rounded-lg" {...props} />,
+                code: (props) => <span className="text-sm bg-secondary rounded text-white font-mono px-1.5 py-0.5" {...props} />,
+                ul: (props) => <ul className="list-disc pl-4 text-white/85" {...props} />,
+                ol: (props) => <ol className="list-decimal pl-4 text-white/85" {...props} />,
+                img: (props) => <img className="md:w-[1000px] relative w-full border rounded-lg" {...props} />,
               }}
             />
           </div>
           <Footer />
-          {/* </div>
-          </ScrollArea> */}
         </article>
-        {/* <div className="w-1/5 right-0 top-120 hidden 2xl:block fixed">
-          <TableOfContents headings={parseHeadings(content)} />
-        </div> */}
       </div>
     </>
   );
