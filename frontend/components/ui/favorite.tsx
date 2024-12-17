42de3fbd6ec772621221201765cd52000e9ad666
Patrick <patrick.solka@mni.thm.de>
2024-12-17 22:06:41 +0100

    Update favorite component

--- frontend/components/ui/favorite.tsx
+++ frontend/components/ui/favorite.tsx
@@ -1,82 +1,3 @@
-/*
-'use client';
-
-import * as React from 'react';
-import { Star } from 'lucide-react';
-import { cn } from '@/lib/utils';
-
-interface FavoriteProps
-  extends React.ComponentPropsWithoutRef<'div'> {
-  checked?: boolean;
-  onCheckedChange?: (checked: boolean) => void;
-}
-
-const Favorite = React.forwardRef<
-  React.ElementRef<'div'>,
-  FavoriteProps
->(({ className, checked = false, onCheckedChange, ...props }, ref) => {
-  const handleClick = () => {
-    onCheckedChange?.(!checked);
-  };
-
-  return (
-    <div
-      ref={ref}
-      className={cn(
-        'h-6 w-6 flex items-center justify-center cursor-pointer text-current',
-        className
-      )}
-      onClick={handleClick}
-      {...props}
-    >
-      <Star
-        size={16}
-        strokeWidth={3}
-        className={cn(
-          checked ? 'text-yellow-500' : 'text-gray-400',
-        )}
-      />
-    </div>
-  );
-});
-
-Favorite.displayName = 'Favorite';
-
-export { Favorite };
-*/
-/*'use client';
-
-import * as React from 'react';
-import { Star } from 'lucide-react';
-import { cn } from '@/lib/utils';
-
-const Favorite = React.forwardRef<
-  React.ElementRef<'div'>,
-  React.ComponentPropsWithoutRef<'div'>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      ref={ref}
-      className={cn(
-        'h-6 w-6 flex items-center justify-center cursor-pointer text-current',
-        className
-      )}
-      {...props}
-    >
-      <Star
-        size={16}
-        strokeWidth={3}
-        className={cn('text-gray-400')}
-      />
-    </div>
-  );
-});
-
-Favorite.displayName = 'Favorite';
-
-export { Favorite };*/
-
-
 'use client';
 
 import * as React from 'react';
@@ -97,7 +18,7 @@ const Favorite = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        'h-6 w-6 flex items-center justify-center cursor-pointer',
+        'h-4 w-4 bg-secondary/30 flex items-center justify-center cursor-pointer',
         className
       )}
       onClick={(e) => {
@@ -107,9 +28,11 @@ const Favorite = React.forwardRef<
       {...props}
     >
       <Star
-        size={16}
-        strokeWidth={3}
-        className={cn(isSelected ? 'text-yellow-400' : 'text-gray-400')}
+        size={18}
+        strokeWidth={1.5}
+        fill={isSelected ? '#FACC15' : 'none'}
+        stroke={isSelected ? '#FACC15' : 'currentColor'}
+        className={isSelected ? 'text-secondary' : 'text-gray-400'}
       />
     </div>
   );
@@ -118,5 +41,9 @@ const Favorite = React.forwardRef<
 Favorite.displayName = 'Favorite';
 
 export { Favorite };
+
+
+
+
 
 
