d7435d7b67094ece8d37bc2e6f59ffde97b9c689
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-01-23 22:59:56 +0900

    fix: small refactor

--- frontend/components/projects/project-card.tsx
+++ frontend/components/projects/project-card.tsx
@@ -1,7 +1,6 @@
 import { ChevronRightIcon } from 'lucide-react';
 import Link from 'next/link';
 
-import Mono from '@/components/ui/mono';
 import { Skeleton } from '@/components/ui/skeleton';
 import { Project, ProjectStats } from '@/lib/workspaces/types';
 
@@ -20,44 +19,46 @@ export default function ProjectCard({
     <Link href={`/project/${project.id}/traces`} key={project.id}>
       <div className="hover:bg-secondary w-96 h-44 rounded-md bg-secondary/40 transition-all duration-100">
         <div className="p-4 flex flex-col gap-2">
-          <div className="flex items-center justify-between mb-auto">
+          <div className="flex items-center justify-between">
             <p className="text-lg font-medium">{project.name}</p>
             <ChevronRightIcon className="w-4 text-secondary-foreground" />
           </div>
           <p className="text-muted-foreground font-mono text-xs">
             {project.id}
           </p>
-          {isLoading ? (
-            <div className="flex gap-8">
-              <div className="flex flex-col gap-2">
-                <Skeleton className="w-5 h-5" />
-                <Skeleton className="w-8 h-6" />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Skeleton className="w-5 h-5" />
-                <Skeleton className="w-8 h-6" />
-              </div>
-              <div className="flex flex-col gap-2">
-                <Skeleton className="w-5 h-5" />
-                <Skeleton className="w-8 h-6" />
-              </div>
-            </div>
-          ) : (
-            <div className="flex gap-8">
-              <div className="flex flex-col gap-1">
-                <p className="text-sm">Spans</p>
-                <Mono className="text-xl">{stats?.spansCount}</Mono>
-              </div>
-              <div className="flex flex-col gap-1">
-                <p className="text-sm">Evaluations</p>
-                <Mono className="text-xl">{stats?.evaluationsCount}</Mono>
-              </div>
-              <div className="flex flex-col gap-1">
-                <p className="text-sm">Datasets</p>
-                <Mono className="text-xl">{stats?.datasetsCount}</Mono>
-              </div>
-            </div>
-          )}
+          <div className="flex gap-8">
+            {isLoading ? (
+              <>
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="w-12 h-5" />
+                  <Skeleton className="w-8 h-6" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="w-12 h-5" />
+                  <Skeleton className="w-8 h-6" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="w-12 h-5" />
+                  <Skeleton className="w-8 h-6" />
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm">Spans</p>
+                  <p className="font-mono text-xl">{stats?.spansCount}</p>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm">Evaluations</p>
+                  <p className="font-mono text-xl">{stats?.evaluationsCount}</p>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <p className="text-sm">Datasets</p>
+                  <p className="font-mono text-xl">{stats?.datasetsCount}</p>
+                </div>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </Link>
