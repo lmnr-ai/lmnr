74bc7f8bb3f577d862d3369f7cb3fa3e86ed5c54
Olzhas Nurpeisov <74583784+olzhik11@users.noreply.github.com>
2025-02-01 14:53:07 +0900

    feat: add restricted access on github auth (#353)
    * feat: add emails whitelist, add sign in check
    
    * feat: add file load logic
    
    * feat: remove log, add toast, error description, fallback
    
    * feat: throw error on wrong file config
    
    * feat: restore grpc files
    
    * Revert "feat: restore grpc files"
    
    This reverts commit c4db1ae175aeccb8bc6cb46756f7c1ff412b35ac.
    
    * feat: replace with promise fileread, add trycatch, fallback

--- frontend/components/sign-in/email-signin.tsx
+++ frontend/components/sign-in/email-signin.tsx
@@ -1,34 +1,24 @@
-'use client';
+"use client";
 
-import { signIn } from 'next-auth/react';
-import { useState } from 'react';
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
-import { Button } from '../ui/button';
-import { Input } from '../ui/input';
-import { Label } from '../ui/label';
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 
 interface EmailSignInProps {
-  showIcon?: boolean;
-  text?: string;
   callbackUrl: string;
-  className?: string;
 }
 
 const validateEmailAddress = (email: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-export function EmailSignInButton({
-  text = 'Sign in',
-  callbackUrl,
-  className,
-  ...props
-}: EmailSignInProps) {
-  const [email, setEmail] = useState('');
+export function EmailSignInButton({ callbackUrl }: EmailSignInProps) {
+  const [email, setEmail] = useState("");
 
   return (
     <div className="h-full flex flex-col space-y-2 mb-2 w-[350px]">
-      <Label className="text-sm text-white text-center">
-        This is a local-only feature. Simply enter any email.
-      </Label>
+      <Label className="text-sm text-white text-center">This is a local-only feature. Simply enter any email.</Label>
       <Input
         type="email"
         placeholder="Email"
@@ -36,18 +26,18 @@ export function EmailSignInButton({
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      {!validateEmailAddress(email) && email
-        && <Label className="text-sm text-white"> Please enter a valid email address </Label>
-      }
+      {!validateEmailAddress(email) && email && (
+        <Label className="text-sm text-white"> Please enter a valid email address </Label>
+      )}
       <Button
         disabled={!email || !validateEmailAddress(email)}
         className="p-4"
-        variant={'light'}
+        variant={"light"}
         onClick={() => {
-          signIn('email', {
-            callbackUrl: callbackUrl,
-            email: email,
-            name: email
+          signIn("email", {
+            callbackUrl,
+            email,
+            name: email,
           });
         }}
         handleEnter
