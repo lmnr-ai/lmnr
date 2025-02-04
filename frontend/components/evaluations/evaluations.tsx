8d44888a26cc48339cc047ca7a6879ccd92c3e40
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-02-04 20:47:35 +0900

    feat: add pagination to evaluations WIP

--- frontend/components/evaluations/evaluations.tsx
+++ frontend/components/evaluations/evaluations.tsx
@@ -1,109 +1,122 @@
-'use client';
+"use client";
 
-import { ColumnDef } from '@tanstack/react-table';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { usePostHog } from 'posthog-js/react';
-import { useState } from 'react';
-import useSWR from 'swr';
+import { ColumnDef } from "@tanstack/react-table";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { usePostHog } from "posthog-js/react";
+import { useMemo, useState } from "react";
+import useSWR from "swr";
 
-import DeleteSelectedRows from '@/components/ui/DeleteSelectedRows';
-import { useProjectContext } from '@/contexts/project-context';
-import { useUserContext } from '@/contexts/user-context';
-import { AggregationFunction } from '@/lib/clickhouse/utils';
-import { Evaluation } from '@/lib/evaluation/types';
-import { Feature, isFeatureEnabled } from '@/lib/features/features';
-import { useToast } from '@/lib/hooks/use-toast';
-import { PaginatedResponse } from '@/lib/types';
-import { swrFetcher } from '@/lib/utils';
+import DeleteSelectedRows from "@/components/ui/DeleteSelectedRows";
+import { useProjectContext } from "@/contexts/project-context";
+import { useUserContext } from "@/contexts/user-context";
+import { AggregationFunction } from "@/lib/clickhouse/utils";
+import { Evaluation } from "@/lib/evaluation/types";
+import { Feature, isFeatureEnabled } from "@/lib/features/features";
+import { useToast } from "@/lib/hooks/use-toast";
+import { PaginatedResponse } from "@/lib/types";
+import { swrFetcher } from "@/lib/utils";
 
-import ClientTimestampFormatter from '../client-timestamp-formatter';
-import { DataTable } from '../ui/datatable';
-import Header from '../ui/header';
-import Mono from '../ui/mono';
-import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '../ui/resizable';
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../ui/select';
-import EvaluationsGroupsBar from './evaluations-groups-bar';
-import ProgressionChart from './progression-chart';
+import ClientTimestampFormatter from "../client-timestamp-formatter";
+import { DataTable } from "../ui/datatable";
+import Header from "../ui/header";
+import Mono from "../ui/mono";
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "../ui/resizable";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import EvaluationsGroupsBar from "./evaluations-groups-bar";
+import ProgressionChart from "./progression-chart";
 
+const columns: ColumnDef<Evaluation>[] = [
+  {
+    accessorKey: "groupId",
+    header: "Group id",
+    size: 120,
+  },
+  {
+    accessorKey: "id",
+    cell: (row) => <Mono>{String(row.getValue())}</Mono>,
+    header: "ID",
+    size: 300,
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+    size: 300,
+  },
+  {
+    header: "Created at",
+    accessorKey: "createdAt",
+    cell: (row) => <ClientTimestampFormatter timestamp={String(row.getValue())} />,
+  },
+];
+
+enum AggregationOptions {
+  AVG = "Average",
+  SUM = "Sum",
+  MIN = "Minimum",
+  MAX = "Maximum",
+  MEDIAN = "Median",
+  p90 = "p90",
+  p95 = "p95",
+  p99 = "p99",
+}
 
 export default function Evaluations() {
   const { projectId } = useProjectContext();
+  const pathName = usePathname();
   const router = useRouter();
+  const { toast } = useToast();
   const searchParams = useSearchParams();
+  const posthog = usePostHog();
+  const { email } = useUserContext();
+
   const { data, mutate } = useSWR<PaginatedResponse<Evaluation>>(
-    `/api/projects/${projectId}/evaluations?groupId=${searchParams.get('groupId')}`,
+    `/api/projects/${projectId}/evaluations?groupId=${searchParams.get("groupId")}`,
     swrFetcher
   );
   const evaluations = data?.items;
 
-  const [aggregationFunction, setAggregationFunction] = useState<AggregationFunction>('AVG');
+  const [aggregationFunction, setAggregationFunction] = useState<AggregationFunction>("AVG");
 
-  const posthog = usePostHog();
-  const { email } = useUserContext();
+  const handleDeleteEvaluations = async (selectedRowIds: string[]) => {
+    try {
+      const response = await fetch(`/api/projects/${projectId}/evaluations?evaluationIds=${selectedRowIds.join(",")}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        await mutate();
+
+        toast({
+          title: "Evaluations deleted",
+          description: `Successfully deleted ${selectedRowIds.length} evaluation(s).`,
+        });
+      } else {
+        throw new Error("Failed to delete evaluations");
+      }
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: "Failed to delete evaluations. Please try again.",
+        variant: "destructive",
+      });
+    }
+  };
 
   if (isFeatureEnabled(Feature.POSTHOG)) {
     posthog.identify(email);
   }
 
-
-  const columns: ColumnDef<Evaluation>[] = [
-    {
-      accessorKey: 'groupId',
-      header: 'Group id',
-      size: 120
-    },
-    {
-      accessorKey: 'id',
-      cell: (row) => <Mono>{String(row.getValue())}</Mono>,
-      header: 'ID',
-      size: 300
-    },
-    {
-      accessorKey: 'name',
-      header: 'Name',
-      size: 300
-    },
-    {
-      header: 'Created at',
-      accessorKey: 'createdAt',
-      cell: (row) => (
-        <ClientTimestampFormatter timestamp={String(row.getValue())} />
-      )
-    }
-  ];
-
-  const { toast } = useToast();
-
-  const handleDeleteEvaluations = async (selectedRowIds: string[]) => {
-    try {
-      const response = await fetch(
-        `/api/projects/${projectId}/evaluations?evaluationIds=${selectedRowIds.join(',')}`,
-        {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        }
-      );
-
-      if (response.ok) {
-        mutate();
-
-        toast({
-          title: 'Evaluations deleted',
-          description: `Successfully deleted ${selectedRowIds.length} evaluation(s).`,
-        });
-      } else {
-        throw new Error('Failed to delete evaluations');
-      }
-    } catch (error) {
-      toast({
-        title: 'Error',
-        description: 'Failed to delete evaluations. Please try again.',
-        variant: 'destructive',
-      });
-    }
-  };
+  const page = useMemo<{ number: number; size: number; count: number }>(() => {
+    const size = searchParams.get("pageSize") ? parseInt(String(searchParams.get("pageSize"))) : 25;
+    return {
+      number: searchParams.get("pageNumber") ? parseInt(String(searchParams.get("pageNumber"))) : 0,
+      size,
+      count: Math.ceil(Number(data?.totalCount || 0) / size),
+    };
+  }, [data?.totalCount, searchParams]);
 
   return (
     <div className="flex flex-col h-full">
@@ -121,14 +134,11 @@ export default function Evaluations() {
                   <SelectValue placeholder="Aggregate" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="AVG">Average</SelectItem>
-                  <SelectItem value="SUM">Sum</SelectItem>
-                  <SelectItem value="MIN">Minimum</SelectItem>
-                  <SelectItem value="MAX">Maximum</SelectItem>
-                  <SelectItem value="MEDIAN">Median</SelectItem>
-                  <SelectItem value="p90">p90</SelectItem>
-                  <SelectItem value="p95">p95</SelectItem>
-                  <SelectItem value="p99">p99</SelectItem>
+                  {(Object.keys(AggregationOptions) as (keyof typeof AggregationOptions)[]).map((option) => (
+                    <SelectItem key={option} value={option}>
+                      {AggregationOptions[option]}
+                    </SelectItem>
+                  ))}
                 </SelectContent>
               </Select>
             </div>
@@ -143,12 +153,20 @@ export default function Evaluations() {
                 enableRowSelection={true}
                 columns={columns}
                 data={evaluations}
-                onRowClick={(row) => {
-                  router.push(`/project/${projectId}/evaluations/${row.original.id}`);
+                onRowClick={(row) => router.push(`/project/${projectId}/evaluations/${row.original.id}`)}
+                defaultPageNumber={page.number}
+                defaultPageSize={page.size}
+                pageCount={page.count}
+                totalItemsCount={data?.totalCount}
+                onPageChange={(pageNumber, pageSize) => {
+                  const params = new URLSearchParams(searchParams.toString());
+                  params.set("pageNumber", pageNumber.toString());
+                  params.set("pageSize", pageSize.toString());
+                  router.push(`${pathName}?${params}`);
                 }}
                 getRowId={(row: Evaluation) => row.id}
                 paginated
-                manualPagination
+                pageSizeOptions={[10, 25]}
                 selectionPanel={(selectedRowIds) => (
                   <div className="flex flex-col space-y-2">
                     <DeleteSelectedRows
--- frontend/components/evaluations/progression-chart.tsx
+++ frontend/components/evaluations/progression-chart.tsx
@@ -1,10 +1,9 @@
 import { Minus } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useMemo, useState } from "react";
+import { useParams, useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 import useSWR from "swr";
 
-import { useProjectContext } from "@/contexts/project-context";
 import { AggregationFunction } from "@/lib/clickhouse/utils";
 import { EvaluationTimeProgression } from "@/lib/evaluation/types";
 import { cn, formatTimestamp, swrFetcher } from "@/lib/utils";
@@ -18,132 +17,117 @@ interface ProgressionChartProps {
   aggregationFunction: AggregationFunction;
 }
 
-export default function ProgressionChart({
-  className,
-  aggregationFunction,
-}: ProgressionChartProps) {
+export default function ProgressionChart({ className, aggregationFunction }: ProgressionChartProps) {
   const [showScores, setShowScores] = useState<string[]>([]);
   const [keys, setKeys] = useState<Set<string>>(new Set());
   const searchParams = new URLSearchParams(useSearchParams().toString());
-  const groupId = searchParams.get('groupId');
-  const { projectId } = useProjectContext();
+  const groupId = searchParams.get("groupId");
+  const params = useParams();
 
-  const { data, error } = useSWR<EvaluationTimeProgression[]>(
-    `/api/projects/${projectId}/evaluation-groups/${groupId}/progression?aggregate=${aggregationFunction}`,
+  const { data, error, isLoading } = useSWR<EvaluationTimeProgression[]>(
+    `/api/projects/${params?.projectId}/evaluation-groups/${groupId}/progression?aggregate=${aggregationFunction}`,
     swrFetcher
   );
+
   useEffect(() => {
-    let newKeys: Set<string> = new Set();
-    data?.forEach(({ names }) => {
-      names.forEach((name) => {
-        newKeys.add(name);
-      });
-    });
+    const newKeys = new Set(data?.flatMap(({ names }) => names) ?? []);
+
     setKeys(newKeys);
+
     if (showScores.length === 0) {
       setShowScores(Array.from(newKeys));
     }
   }, [data]);
 
-  const convertedScores = useMemo(() =>
-    data?.map(({ timestamp, evaluationId, names, values }) => ({
-      timestamp,
-      evaluationId,
-      ...Object.fromEntries(names.map((name, index) => ([name, values[index]]))),
-    })) ?? [], [data]
+  const convertedScores = useMemo(
+    () =>
+      data?.map(({ timestamp, evaluationId, names, values }) => ({
+        timestamp,
+        evaluationId,
+        ...Object.fromEntries(names.map((name, index) => [name, values[index]])),
+      })) ?? [],
+    [data]
   );
 
-  const chartConfig = Object.fromEntries(Array.from(keys).map((key, index) => ([
-    key, {
-      color: `hsl(var(--chart-${index % 5 + 1}))`,
-      label: key,
-    }
-  ]))) satisfies ChartConfig;
+  const chartConfig = Object.fromEntries(
+    Array.from(keys).map((key, index) => [
+      key,
+      {
+        color: `hsl(var(--chart-${(index % 5) + 1}))`,
+        label: key,
+      },
+    ])
+  ) satisfies ChartConfig;
 
   const horizontalPadding = Math.max(10 - (data?.length ?? 0), 0) * 50;
 
+  const handleClick = useCallback((key: string) => {
+    setShowScores((prevScores) =>
+      prevScores.includes(key) ? prevScores.filter((score) => score !== key) : [...prevScores, key]
+    );
+  }, []);
+
   return (
-    <div className={cn('w-full h-full', className)}>
-      <ChartContainer
-        config={chartConfig as ChartConfig}
-        className={cn('h-5/6', 'w-full')}
-      >
-        {!data || error ? (
+    <div className={cn("w-full h-full", className)}>
+      <ChartContainer config={chartConfig as ChartConfig} className={cn("h-5/6", "w-full")}>
+        {!data && isLoading ? (
           <div className="h-full w-full">
             <Skeleton className="h-full w-full" />
           </div>
-        ) : <LineChart
-          margin={{ top: 10, right: 10, bottom: 5, left: -12 }}
-          accessibilityLayer
-          data={convertedScores}
-        >
-          <CartesianGrid vertical={false} />
-          <XAxis
-            type="category"
-            dataKey="timestamp"
-            tickLine={false}
-            axisLine={false}
-            // tickFormatter={(value: number) => formatTimestamp(`${value}Z`)}
-            tick={false}
-            height={8}
-            padding={{ left: horizontalPadding, right: horizontalPadding }}
-          />
-          <YAxis
-            tickLine={false}
-            axisLine={false}
-            tickMargin={8}
-            tickCount={5}
-          />
-          <ChartTooltip
-            cursor={false}
-            content={
-              <ChartTooltipContent
-                className="min-w-60"
-                labelFormatter={(value) => formatTimestamp(`${value}Z`)}
-              />
-            }
-          />
-          {Array.from(keys).filter((key) => showScores.includes(key)).map((key) => (
-            <Line
-              dot={{
-                stroke: chartConfig[key].color,
-                strokeWidth: 4,
-                r: 2,
-              }}
-              dataKey={key}
-              stroke={chartConfig[key].color}
-              key={key}
-              isAnimationActive={false}
+        ) : (
+          <LineChart margin={{ top: 10, right: 10, bottom: 5, left: -12 }} accessibilityLayer data={convertedScores}>
+            <CartesianGrid vertical={false} />
+            <XAxis
+              type="category"
+              dataKey="timestamp"
+              tickLine={false}
+              axisLine={false}
+              // tickFormatter={(value: number) => formatTimestamp(`${value}Z`)}
+              tick={false}
+              height={8}
+              padding={{ left: horizontalPadding, right: horizontalPadding }}
             />
-          ))}
-        </LineChart>
-        }
+            <YAxis tickLine={false} axisLine={false} tickMargin={8} tickCount={5} />
+            <ChartTooltip
+              cursor={false}
+              content={
+                <ChartTooltipContent className="min-w-60" labelFormatter={(value) => formatTimestamp(`${value}Z`)} />
+              }
+            />
+            {Array.from(keys)
+              .filter((key) => showScores.includes(key))
+              .map((key) => (
+                <Line
+                  dot={{
+                    stroke: chartConfig[key].color,
+                    strokeWidth: 4,
+                    r: 2,
+                  }}
+                  dataKey={key}
+                  stroke={chartConfig[key].color}
+                  key={key}
+                  isAnimationActive={false}
+                />
+              ))}
+          </LineChart>
+        )}
       </ChartContainer>
       <div className="flex flex-row justify-center w-full space-x-2 items-center">
         {Array.from(keys).map((key) => (
           <div
             key={key}
-            className={
-              "flex items-center text-sm cursor-pointer " +
-              "decoration-dashed text-muted-foreground"
+            className="flex items-center text-sm cursor-pointer decoration-dashed text-muted-foreground"
+            style={
+              showScores.includes(key)
+                ? {
+                    color: chartConfig[key].color,
+                  }
+                : {}
             }
-            style={showScores.includes(key) ? {
-              color: chartConfig[key].color,
-            } : {}}
-            onClick={() => {
-              let newShowScores = new Set(showScores);
-              if (newShowScores.has(key)) {
-                newShowScores.delete(key);
-              } else {
-                newShowScores.add(key);
-              }
-              setShowScores(Array.from(newShowScores));
-            }}
+            onClick={() => handleClick(key)}
           >
             <Minus className="h-4 w-4 mt-1" />
-            <Label className="cursor-pointer">
-              {key}
-            </Label>
+            <Label className="cursor-pointer">{key}</Label>
           </div>
         ))}
       </div>
