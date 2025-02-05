42dec5c9d25394e6955978d3d0803a1ed73548d9
Din <dinmukhamed.mailibay@gmail.com>
2025-02-05 14:10:25 +0500

    hotfix: remove realtime related stuff from migrations

--- frontend/lib/db/migrations/0019_hesitant_shatterstar.sql
+++ frontend/lib/db/migrations/0019_hesitant_shatterstar.sql
@@ -1,4 +1,2 @@
 ALTER TYPE "public"."span_type" ADD VALUE 'TOOL';--> statement-breakpoint
 ALTER TABLE "spans" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
-CREATE POLICY "select_by_next_api_key" ON "spans" AS PERMISSIVE FOR SELECT TO public USING (is_project_id_accessible_for_api_key(api_key(), project_id));--> statement-breakpoint
-ALTER POLICY "select_by_next_api_key" ON "traces" TO anon,authenticated USING (is_project_id_accessible_for_api_key(api_key(), project_id));
