16e0714fb7a1fc6340fe2228dacbb836e343a741
Din <dinmukhamed.mailibay@gmail.com>
2025-03-07 21:08:16 +0500

    linting

--- frontend/lib/db/migrations/relations.ts
+++ frontend/lib/db/migrations/relations.ts
@@ -1,242 +1,243 @@
 import { relations } from "drizzle-orm/relations";
-import { projects, renderTemplates, users, apiKeys, labelClasses, labelingQueues, labelingQueueItems, spans, events, labelClassesForPath, evaluationResults, evaluationScores, datasets, pipelines, targetPipelineVersions, pipelineVersions, workspaces, providerApiKeys, subscriptionTiers, traces, playgrounds, userSubscriptionInfo, datasetDatapoints, evaluations, membersOfWorkspaces, projectApiKeys, workspaceUsage, labels, machines, datapointToSpan } from "./schema";
+
+import { apiKeys, datapointToSpan,datasetDatapoints, datasets, evaluationResults, evaluations, evaluationScores, events, labelClasses, labelClassesForPath, labelingQueueItems, labelingQueues, labels, machines, membersOfWorkspaces, pipelines, pipelineVersions, playgrounds, projectApiKeys, projects, providerApiKeys, renderTemplates, spans, subscriptionTiers, targetPipelineVersions, traces, users, userSubscriptionInfo, workspaces, workspaceUsage } from "./schema";
 
 export const renderTemplatesRelations = relations(renderTemplates, ({one}) => ({
-	project: one(projects, {
-		fields: [renderTemplates.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [renderTemplates.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const projectsRelations = relations(projects, ({one, many}) => ({
-	renderTemplates: many(renderTemplates),
-	labelClasses: many(labelClasses),
-	labelClassesForPaths: many(labelClassesForPath),
-	labelingQueues: many(labelingQueues),
-	datasets: many(datasets),
-	workspace: one(workspaces, {
-		fields: [projects.workspaceId],
-		references: [workspaces.id]
-	}),
-	providerApiKeys: many(providerApiKeys),
-	traces: many(traces),
-	playgrounds: many(playgrounds),
-	pipelines: many(pipelines),
-	evaluations: many(evaluations),
-	projectApiKeys: many(projectApiKeys),
-	machines: many(machines),
-	spans: many(spans),
+  renderTemplates: many(renderTemplates),
+  labelClasses: many(labelClasses),
+  labelClassesForPaths: many(labelClassesForPath),
+  labelingQueues: many(labelingQueues),
+  datasets: many(datasets),
+  workspace: one(workspaces, {
+    fields: [projects.workspaceId],
+    references: [workspaces.id]
+  }),
+  providerApiKeys: many(providerApiKeys),
+  traces: many(traces),
+  playgrounds: many(playgrounds),
+  pipelines: many(pipelines),
+  evaluations: many(evaluations),
+  projectApiKeys: many(projectApiKeys),
+  machines: many(machines),
+  spans: many(spans),
 }));
 
 export const apiKeysRelations = relations(apiKeys, ({one}) => ({
-	user: one(users, {
-		fields: [apiKeys.userId],
-		references: [users.id]
-	}),
+  user: one(users, {
+    fields: [apiKeys.userId],
+    references: [users.id]
+  }),
 }));
 
 export const usersRelations = relations(users, ({many}) => ({
-	apiKeys: many(apiKeys),
-	userSubscriptionInfos: many(userSubscriptionInfo),
-	membersOfWorkspaces: many(membersOfWorkspaces),
+  apiKeys: many(apiKeys),
+  userSubscriptionInfos: many(userSubscriptionInfo),
+  membersOfWorkspaces: many(membersOfWorkspaces),
 }));
 
 export const labelClassesRelations = relations(labelClasses, ({one, many}) => ({
-	project: one(projects, {
-		fields: [labelClasses.projectId],
-		references: [projects.id]
-	}),
-	labels: many(labels),
+  project: one(projects, {
+    fields: [labelClasses.projectId],
+    references: [projects.id]
+  }),
+  labels: many(labels),
 }));
 
 export const labelingQueueItemsRelations = relations(labelingQueueItems, ({one}) => ({
-	labelingQueue: one(labelingQueues, {
-		fields: [labelingQueueItems.queueId],
-		references: [labelingQueues.id]
-	}),
+  labelingQueue: one(labelingQueues, {
+    fields: [labelingQueueItems.queueId],
+    references: [labelingQueues.id]
+  }),
 }));
 
 export const labelingQueuesRelations = relations(labelingQueues, ({one, many}) => ({
-	labelingQueueItems: many(labelingQueueItems),
-	project: one(projects, {
-		fields: [labelingQueues.projectId],
-		references: [projects.id]
-	}),
+  labelingQueueItems: many(labelingQueueItems),
+  project: one(projects, {
+    fields: [labelingQueues.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const eventsRelations = relations(events, ({one}) => ({
-	span: one(spans, {
-		fields: [events.spanId],
-		references: [spans.spanId]
-	}),
+  span: one(spans, {
+    fields: [events.spanId],
+    references: [spans.spanId]
+  }),
 }));
 
 export const spansRelations = relations(spans, ({one, many}) => ({
-	events: many(events),
-	datapointToSpans: many(datapointToSpan),
-	project: one(projects, {
-		fields: [spans.projectId],
-		references: [projects.id]
-	}),
+  events: many(events),
+  datapointToSpans: many(datapointToSpan),
+  project: one(projects, {
+    fields: [spans.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const labelClassesForPathRelations = relations(labelClassesForPath, ({one}) => ({
-	project: one(projects, {
-		fields: [labelClassesForPath.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [labelClassesForPath.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const evaluationScoresRelations = relations(evaluationScores, ({one}) => ({
-	evaluationResult: one(evaluationResults, {
-		fields: [evaluationScores.resultId],
-		references: [evaluationResults.id]
-	}),
+  evaluationResult: one(evaluationResults, {
+    fields: [evaluationScores.resultId],
+    references: [evaluationResults.id]
+  }),
 }));
 
 export const evaluationResultsRelations = relations(evaluationResults, ({one, many}) => ({
-	evaluationScores: many(evaluationScores),
-	evaluation: one(evaluations, {
-		fields: [evaluationResults.evaluationId],
-		references: [evaluations.id]
-	}),
+  evaluationScores: many(evaluationScores),
+  evaluation: one(evaluations, {
+    fields: [evaluationResults.evaluationId],
+    references: [evaluations.id]
+  }),
 }));
 
 export const datasetsRelations = relations(datasets, ({one, many}) => ({
-	project: one(projects, {
-		fields: [datasets.projectId],
-		references: [projects.id]
-	}),
-	datasetDatapoints: many(datasetDatapoints),
+  project: one(projects, {
+    fields: [datasets.projectId],
+    references: [projects.id]
+  }),
+  datasetDatapoints: many(datasetDatapoints),
 }));
 
 export const targetPipelineVersionsRelations = relations(targetPipelineVersions, ({one}) => ({
-	pipeline: one(pipelines, {
-		fields: [targetPipelineVersions.pipelineId],
-		references: [pipelines.id]
-	}),
-	pipelineVersion: one(pipelineVersions, {
-		fields: [targetPipelineVersions.pipelineVersionId],
-		references: [pipelineVersions.id]
-	}),
+  pipeline: one(pipelines, {
+    fields: [targetPipelineVersions.pipelineId],
+    references: [pipelines.id]
+  }),
+  pipelineVersion: one(pipelineVersions, {
+    fields: [targetPipelineVersions.pipelineVersionId],
+    references: [pipelineVersions.id]
+  }),
 }));
 
 export const pipelinesRelations = relations(pipelines, ({one, many}) => ({
-	targetPipelineVersions: many(targetPipelineVersions),
-	project: one(projects, {
-		fields: [pipelines.projectId],
-		references: [projects.id]
-	}),
+  targetPipelineVersions: many(targetPipelineVersions),
+  project: one(projects, {
+    fields: [pipelines.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const pipelineVersionsRelations = relations(pipelineVersions, ({many}) => ({
-	targetPipelineVersions: many(targetPipelineVersions),
+  targetPipelineVersions: many(targetPipelineVersions),
 }));
 
 export const workspacesRelations = relations(workspaces, ({one, many}) => ({
-	projects: many(projects),
-	subscriptionTier: one(subscriptionTiers, {
-		fields: [workspaces.tierId],
-		references: [subscriptionTiers.id]
-	}),
-	membersOfWorkspaces: many(membersOfWorkspaces),
-	workspaceUsages: many(workspaceUsage),
+  projects: many(projects),
+  subscriptionTier: one(subscriptionTiers, {
+    fields: [workspaces.tierId],
+    references: [subscriptionTiers.id]
+  }),
+  membersOfWorkspaces: many(membersOfWorkspaces),
+  workspaceUsages: many(workspaceUsage),
 }));
 
 export const providerApiKeysRelations = relations(providerApiKeys, ({one}) => ({
-	project: one(projects, {
-		fields: [providerApiKeys.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [providerApiKeys.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const subscriptionTiersRelations = relations(subscriptionTiers, ({many}) => ({
-	workspaces: many(workspaces),
+  workspaces: many(workspaces),
 }));
 
 export const tracesRelations = relations(traces, ({one}) => ({
-	project: one(projects, {
-		fields: [traces.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [traces.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const playgroundsRelations = relations(playgrounds, ({one}) => ({
-	project: one(projects, {
-		fields: [playgrounds.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [playgrounds.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const userSubscriptionInfoRelations = relations(userSubscriptionInfo, ({one}) => ({
-	user: one(users, {
-		fields: [userSubscriptionInfo.userId],
-		references: [users.id]
-	}),
+  user: one(users, {
+    fields: [userSubscriptionInfo.userId],
+    references: [users.id]
+  }),
 }));
 
 export const datasetDatapointsRelations = relations(datasetDatapoints, ({one, many}) => ({
-	dataset: one(datasets, {
-		fields: [datasetDatapoints.datasetId],
-		references: [datasets.id]
-	}),
-	datapointToSpans: many(datapointToSpan),
+  dataset: one(datasets, {
+    fields: [datasetDatapoints.datasetId],
+    references: [datasets.id]
+  }),
+  datapointToSpans: many(datapointToSpan),
 }));
 
 export const evaluationsRelations = relations(evaluations, ({one, many}) => ({
-	evaluationResults: many(evaluationResults),
-	project: one(projects, {
-		fields: [evaluations.projectId],
-		references: [projects.id]
-	}),
+  evaluationResults: many(evaluationResults),
+  project: one(projects, {
+    fields: [evaluations.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const membersOfWorkspacesRelations = relations(membersOfWorkspaces, ({one}) => ({
-	user: one(users, {
-		fields: [membersOfWorkspaces.userId],
-		references: [users.id]
-	}),
-	workspace: one(workspaces, {
-		fields: [membersOfWorkspaces.workspaceId],
-		references: [workspaces.id]
-	}),
+  user: one(users, {
+    fields: [membersOfWorkspaces.userId],
+    references: [users.id]
+  }),
+  workspace: one(workspaces, {
+    fields: [membersOfWorkspaces.workspaceId],
+    references: [workspaces.id]
+  }),
 }));
 
 export const projectApiKeysRelations = relations(projectApiKeys, ({one}) => ({
-	project: one(projects, {
-		fields: [projectApiKeys.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [projectApiKeys.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const workspaceUsageRelations = relations(workspaceUsage, ({one}) => ({
-	workspace: one(workspaces, {
-		fields: [workspaceUsage.workspaceId],
-		references: [workspaces.id]
-	}),
+  workspace: one(workspaces, {
+    fields: [workspaceUsage.workspaceId],
+    references: [workspaces.id]
+  }),
 }));
 
 export const labelsRelations = relations(labels, ({one}) => ({
-	labelClass: one(labelClasses, {
-		fields: [labels.classId],
-		references: [labelClasses.id]
-	}),
+  labelClass: one(labelClasses, {
+    fields: [labels.classId],
+    references: [labelClasses.id]
+  }),
 }));
 
 export const machinesRelations = relations(machines, ({one}) => ({
-	project: one(projects, {
-		fields: [machines.projectId],
-		references: [projects.id]
-	}),
+  project: one(projects, {
+    fields: [machines.projectId],
+    references: [projects.id]
+  }),
 }));
 
 export const datapointToSpanRelations = relations(datapointToSpan, ({one}) => ({
-	datasetDatapoint: one(datasetDatapoints, {
-		fields: [datapointToSpan.datapointId],
-		references: [datasetDatapoints.id]
-	}),
-	span: one(spans, {
-		fields: [datapointToSpan.spanId],
-		references: [spans.spanId]
-	}),
+  datasetDatapoint: one(datasetDatapoints, {
+    fields: [datapointToSpan.datapointId],
+    references: [datasetDatapoints.id]
+  }),
+  span: one(spans, {
+    fields: [datapointToSpan.spanId],
+    references: [spans.spanId]
+  }),
 }));
