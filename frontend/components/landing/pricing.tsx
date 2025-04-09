83f28ab9dcc8b03bdff23962ecc2503f8690d084
Robert Kim <skull8888888@gmail.com>
2025-04-10 00:58:03 +0100

    new copy

--- frontend/components/landing/pricing.tsx
+++ frontend/components/landing/pricing.tsx
@@ -21,12 +21,12 @@ import PricingCard from './pricing-card';
 export default function Pricing() {
   const posthog = usePostHog();
 
-  const [spanCount, setSpanCount] = useState(100);  // in thousands
+  const [spanCount, setSpanCount] = useState(200);  // in thousands
   const [teamMembers, setTeamMembers] = useState(3);
 
   const calculateProPrice = () => {
-    const basePrice = 49;
-    const additionalSpansCost = spanCount > 100 ? Math.floor((spanCount - 100) / 100 * 25) : 0;
+    const basePrice = 50;
+    const additionalSpansCost = spanCount > 200 ? Math.floor((spanCount - 200) / 100 * 5) : 0;
     const additionalMembersCost = (teamMembers - 3) * 25;
     return basePrice + additionalSpansCost + additionalMembersCost;
   };
@@ -34,6 +34,20 @@ export default function Pricing() {
   const handleQuestionClick = (question: string) => {
     posthog?.capture('faq_question_clicked', { question });
   };
+
+  // Add FAQ data
+  const faqItems = [
+    {
+      id: 'span',
+      question: 'What is a span?',
+      answer: 'A span represents a unit of work or operation in you application. In the context of tracing, single LLM call or function tool call is a span. In case of evaluations, executor run and evaluator run are spans.'
+    },
+    {
+      id: 'agent-steps',
+      question: 'What is an agent step?',
+      answer: 'An agent step is a single step of an execution of the Index browser agent when it is called via API.'
+    }
+  ];
 
   return (
     <div className="flex flex-col items-center mt-32 w-full h-full">
@@ -45,9 +59,10 @@ export default function Pricing() {
             price="0 / month"
             features={[
               '50K spans / month',
-              '30 day data retention',
+              '15 day data retention',
               '1 team member',
-              'Community support'
+              '100 agent steps / month',
+              'Community support',
             ]}
           />
           <Link href="/projects">
@@ -56,81 +71,21 @@ export default function Pricing() {
             </Button>
           </Link>
         </div>
-        <div
-          className="rounded relative"
-        >
-          <div className="absolute inset-0 z-10 overflow-hidden rounded-lg">
-            <Image
-              src={noise}
-              alt=""
-              className="w-full h-full object-cover object-top"
-            />
-          </div>
-          <div className="bg-transparent h-full w-full rounded p-8 flex flex-col z-20">
-            <PricingCard
-              className="text-white z-20"
-              title="Pro"
-              price={`$${calculateProPrice()} / month`}
-              features={[
-                '100k spans / month included',
-                '90 day data retention',
-                '3 team members included',
-                'Priority support'
-              ]}
-              subfeatures={[
-                'then $25 per 100k of additional spans',
-                null,
-                '$25 per additional team member',
-                null
-              ]}
-            />
-            <div className="space-y-4 z-20 flex flex-col">
-              <div className="space-y-2">
-                <div className="text-white">Spans per month {spanCount}k</div>
-                <Slider
-                  defaultValue={[100]}
-                  max={1000}
-                  min={100}
-                  step={100}
-                  onValueChange={(value) => setSpanCount(value[0])}
-                />
-              </div>
-              <div className="space-y-2">
-                <div className="text-white">Team members {teamMembers}</div>
-                <Slider
-                  defaultValue={[3]}
-                  max={10}
-                  min={3}
-                  step={1}
-                  onValueChange={(value) => setTeamMembers(value[0])}
-                />
-              </div>
-              <Link href="/projects" className="w-full z-20">
-                <Button
-                  className="h-10 text-base bg-white/90 text-black hover:bg-white/70 w-full"
-                  variant="outline"
-                >
-                  Get started
-                </Button>
-              </Link>
-            </div>
-          </div>
-        </div>
         <div className="p-8 border rounded-lg flex flex-col space-y-4">
           <PricingCard
             className="text-secondary-foreground"
-            title="Team"
-            price="$399 / month"
+            title="Hobby"
+            price="$25 / month"
             features={[
-              '1M spans / month',
-              '180 day data retention',
-              '10 team members included',
-              'Private Slack channel'
+              '100k spans / month',
+              '30 day data retention',
+              '2 team members',
+              '1000 agent steps / month',
+              'Community support',
             ]}
             subfeatures={[
-              'then $25 per 100k of additional spans',
+              'then $5 per 100k of additional spans',
               null,
-              '$25 per additional team member',
               null
             ]}
           />
@@ -140,26 +95,75 @@ export default function Pricing() {
             </Button>
           </Link>
         </div>
+        <div className="h-full w-full rounded p-8 flex flex-col z-20 border border-primary bg-primary">
+          <PricingCard
+            className="text-white z-20"
+            title="Pro"
+            price={`$${calculateProPrice()} / month`}
+            features={[
+              '200k spans / month included',
+              '90 day data retention',
+              '3 team members included',
+              '3000 agent steps / month',
+              'Private Slack channel',
+            ]}
+            subfeatures={[
+              'then $5 per 100k of additional spans',
+              null,
+              '$25 per additional team member',
+              null
+            ]}
+          />
+          <div className="space-y-4 z-20 flex flex-col">
+            <div className="space-y-2">
+              <div className="text-white">Spans per month {spanCount}k</div>
+              <Slider
+                defaultValue={[200]}
+                max={1000}
+                min={200}
+                step={100}
+                onValueChange={(value) => setSpanCount(value[0])}
+              />
+            </div>
+            <div className="space-y-2">
+              <div className="text-white">Team members {teamMembers}</div>
+              <Slider
+                defaultValue={[3]}
+                max={10}
+                min={3}
+                step={1}
+                onValueChange={(value) => setTeamMembers(value[0])}
+              />
+            </div>
+            <Link href="/projects" className="w-full z-20">
+              <Button
+                className="h-10 text-base bg-white/90 border-none text-primary hover:bg-white/70 w-full"
+                variant="outline"
+              >
+                Get started
+              </Button>
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="w-full max-w-3xl mt-16 mb-32 px-4">
         <h2 className="text-2xl font-bold mb-4 text-center">
           Frequently Asked Questions
         </h2>
         <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger
-              className="text-2xl"
-              onClick={() => handleQuestionClick('What is a span?')}
-            >
-              What is a span?
-            </AccordionTrigger>
-            <AccordionContent className="text-secondary-foreground">
-              A span represents a unit of work or operation in you application.
-              In the context of tracing, single LLM call or function tool call
-              is a span. In case of evaluations, executor run and evaluator run
-              are spans.
-            </AccordionContent>
-          </AccordionItem>
+          {faqItems.map((item) => (
+            <AccordionItem key={item.id} value={item.id}>
+              <AccordionTrigger
+                className="text-2xl"
+                onClick={() => handleQuestionClick(item.question)}
+              >
+                {item.question}
+              </AccordionTrigger>
+              <AccordionContent className="text-secondary-foreground">
+                {item.answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
         </Accordion>
       </div>
       <div className="flex-grow"></div>
