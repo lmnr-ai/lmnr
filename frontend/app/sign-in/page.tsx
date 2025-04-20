da802f82244b5a3688182bdbe3e3d4e2b43a95cb
Robert Kim <skull8888888@gmail.com>
2025-04-20 01:02:08 +0100

    sign in page ui

--- frontend/app/sign-in/page.tsx
+++ frontend/app/sign-in/page.tsx
@@ -24,39 +24,41 @@ export default async function SignInPage(props: {
     redirect(callbackUrl);
   }
   return (
-    <div className="flex h-full items-center justify-center">
-      <div className="flex flex-col items-center relative bg-primary rounded-lg">
-        {/*<div className="inset-0 absolute z-10 md:rounded-lg overflow-hidden">*/}
-        {/*  <Image src={noise} alt="" className="w-full h-full" priority quality={100} />*/}
-        {/*</div>*/}
-        <div className="z-20 flex flex-col items-center p-16 px-8">
-          <Image alt="" src={logo} width={220} className="my-16" />
-          {isFeatureEnabled(Feature.EMAIL_AUTH) && <EmailSignInButton callbackUrl={callbackUrl} />}
-          {isFeatureEnabled(Feature.GOOGLE_AUTH) && (
-            <GoogleSignInButton className="text-[16px] py-6 px-4 pr-8 mt-4" callbackUrl={callbackUrl} />
-          )}
-          {isFeatureEnabled(Feature.GITHUB_AUTH) && (
-            <GitHubSignInButton
-              className={cn("text-[16px] py-6 px-4 pr-8 mt-4", {
-                "w-full": isFeatureEnabled(Feature.EMAIL_AUTH),
-              })}
-              callbackUrl={callbackUrl}
-            />
-          )}
-          {!isFeatureEnabled(Feature.EMAIL_AUTH) && (
-            <div className="mt-16 text-sm text-secondary-foreground">
-              By continuing you agree to our{" "}
-              <a href="https://docs.lmnr.ai/policies/privacy-policy" target="_blank" className="underline">
-                Privacy Policy
-              </a>{" "}
-              and{" "}
-              <a href="https://docs.lmnr.ai/policies/terms-of-service" target="_blank" className="underline">
-                Terms of Service
-              </a>
-            </div>
-          )}
+    <div className="flex flex-col h-full">
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex flex-col items-center relative rounded-lg">
+          <div className="z-20 flex flex-col items-center p-8 w-[400px]">
+            <Image alt="Logo" src={logo} width={200} className="mb-16" />
+
+            {isFeatureEnabled(Feature.EMAIL_AUTH) && <EmailSignInButton callbackUrl={callbackUrl} />}
+            {isFeatureEnabled(Feature.GOOGLE_AUTH) && (
+              <GoogleSignInButton className="text-[16px] py-6 px-4 pr-8 mt-4 w-full" callbackUrl={callbackUrl} />
+            )}
+            {isFeatureEnabled(Feature.GITHUB_AUTH) && (
+              <GitHubSignInButton
+                className={cn("text-[16px] py-6 px-4 pr-8 mt-4 w-full", {
+                  "w-full": isFeatureEnabled(Feature.EMAIL_AUTH),
+                })}
+                callbackUrl={callbackUrl}
+              />
+            )}
+          </div>
         </div>
       </div>
+      <footer className="p-6 flex justify-center">
+        {!isFeatureEnabled(Feature.EMAIL_AUTH) && (
+          <div className="text-sm font-medium text-white/70">
+            By continuing you agree to our{" "}
+            <a href="https://docs.lmnr.ai/policies/privacy-policy" target="_blank" className="text-white">
+              Privacy Policy
+            </a>{" "}
+            and{" "}
+            <a href="https://docs.lmnr.ai/policies/terms-of-service" target="_blank" className="text-white">
+              Terms of Service
+            </a>
+          </div>
+        )}
+      </footer>
     </div>
   );
 }
