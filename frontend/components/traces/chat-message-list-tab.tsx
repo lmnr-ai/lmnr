f7b86b5c66e2996c16c3fd2f062871d2dc577b01
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-04-25 10:37:09 +0100

    feat: fix index

--- frontend/components/traces/chat-message-list-tab.tsx
+++ frontend/components/traces/chat-message-list-tab.tsx
@@ -1,10 +1,8 @@
 import { useVirtualizer } from "@tanstack/react-virtual";
-import { ChevronDown } from "lucide-react";
 import { memo, useMemo, useRef } from "react";
 
 import ImageWithPreview from "@/components/playground/image-with-preview";
 import CodeHighlighter from "@/components/traces/code-highlighter";
-import { Button } from "@/components/ui/button";
 import { ChatMessage, ChatMessageContentPart, flattenContentOfMessages, OpenAIImageUrl } from "@/lib/types";
 
 import DownloadButton from "../ui/download-button";
@@ -99,66 +97,66 @@ function PureChatMessageListTab({ messages, presetKey }: ChatMessageListTabProps
     count: memoizedMessages.length,
     getScrollElement: () => parentRef.current,
     estimateSize: () => 500,
-    overscan: 5,
+    overscan: 3,
     gap: 16,
   });
 
   const items = virtualizer.getVirtualItems();
 
   return (
-    <div className="relative h-full">
+    // <div className="relative h-full">
+    <div
+      ref={parentRef}
+      className="List h-full overflow-y-auto"
+      style={{
+        width: "100%",
+        contain: "strict",
+      }}
+    >
       <div
-        ref={parentRef}
-        className="List h-full overflow-y-auto"
         style={{
+          height: virtualizer.getTotalSize(),
           width: "100%",
-          contain: "strict",
+          position: "relative",
         }}
       >
         <div
           style={{
-            height: virtualizer.getTotalSize(),
+            position: "absolute",
+            top: 0,
+            left: 0,
             width: "100%",
-            position: "relative",
+            transform: `translateY(${items[0]?.start ?? 0}px)`,
           }}
         >
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              transform: `translateY(${items[0]?.start ?? 0}px)`,
-            }}
-          >
-            {items.map((virtualRow, index) => {
-              const message = memoizedMessages[index];
-              return (
-                <div
-                  key={virtualRow.key}
-                  ref={virtualizer.measureElement}
-                  data-index={virtualRow.index}
-                  className="flex flex-col border rounded mb-4"
-                >
-                  <div className="font-medium text-sm text-secondary-foreground border-b p-2">
-                    {message.role.toUpperCase()}
-                  </div>
-                  <ContentParts presetKey={presetKey} contentParts={message.content} />
+          {items.map((virtualRow) => {
+            const message = memoizedMessages[virtualRow.index];
+            return (
+              <div
+                key={virtualRow.key}
+                ref={virtualizer.measureElement}
+                data-index={virtualRow.index}
+                className="flex flex-col border rounded mb-4"
+              >
+                <div className="font-medium text-sm text-secondary-foreground border-b p-2">
+                  {message.role.toUpperCase()}
                 </div>
-              );
-            })}
-          </div>
+                <ContentParts presetKey={presetKey} contentParts={message.content} />
+              </div>
+            );
+          })}
         </div>
       </div>
-      <Button
-        variant="outline"
-        size="icon"
-        className="absolute bottom-3 right-3 rounded-full"
-        onClick={() => virtualizer.scrollToIndex(virtualizer.options.count - 1, { align: "end", behavior: "smooth" })}
-      >
-        <ChevronDown className="w-4 h-4" />
-      </Button>
     </div>
+    // <Button
+    //   variant="outline"
+    //   size="icon"
+    //   className="absolute bottom-3 right-3 rounded-full"
+    //   onClick={() => virtualizer.scrollToIndex(memoizedMessages.length - 1, { align: "end" })}
+    // >
+    //   <ChevronDown className="w-4 h-4" />
+    // </Button>
+    // </div>
   );
 }
 
