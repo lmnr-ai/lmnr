503d63a9132fb7321bcdd52a96046a0386f51681
Robert Kim <skull8888888@gmail.com>
2025-07-07 17:57:38 +0100

    allocator (#687)

--- app-server/src/main.rs
+++ app-server/src/main.rs
@@ -1,3 +1,11 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
+
 use actix_web::{
     App, HttpServer,
     middleware::{Logger, NormalizePath},
