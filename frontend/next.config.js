d32ec8199d2c746b82c4825967ba1dd8c9b86e3f
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-07-16 22:01:31 +0100

    feat: increase max age

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -33,7 +33,7 @@ const nextConfig = {
           },
           {
             key: "Strict-Transport-Security",
-            value: "max-age=7776000; includeSubDomains; preload",
+            value: "max-age=31536000; includeSubDomains; preload",
           },
           {
             key: "Permissions-Policy",
