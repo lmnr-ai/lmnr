0c4b8631d9670f7809f0cfc21f0950b7eb8b7c48
Dinmukhamed Mailibay <47117969+dinmukhamedm@users.noreply.github.com>
2025-07-17 12:54:24 +0100

    Update to lapin v3, some other deps + small fixes (#711)
    * upgrade lapin to v3, update some other deps
    
    * dont clone large span input, remove async_trait
    
    * bump rust to 1.86

--- app-server/src/storage/mock.rs
+++ app-server/src/storage/mock.rs
@@ -2,7 +2,6 @@ use anyhow::Result;
 
 pub struct MockStorage;
 
-#[async_trait::async_trait]
 impl super::StorageTrait for MockStorage {
     async fn store(&self, _data: Vec<u8>, _key: &str) -> Result<String> {
         Ok("mock".to_string())
