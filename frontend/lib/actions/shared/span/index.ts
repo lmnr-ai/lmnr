b2d9cba2d345f6eef74f682f2d1e3d1167b57c3b
Din <dinmukhamed.mailibay@gmail.com>
2025-08-18 13:32:57 +0100

    query span input outputs from clickhouse for shared

--- frontend/lib/actions/shared/span/index.ts
+++ frontend/lib/actions/shared/span/index.ts
@@ -1,6 +1,7 @@
 import { and, asc, eq } from "drizzle-orm";
 import { z } from "zod/v4";
 
+import { clickhouseClient } from "@/lib/clickhouse/client";
 import { db } from "@/lib/db/drizzle";
 import { events, spans } from "@/lib/db/migrations/schema";
 
@@ -12,15 +13,49 @@ export const GetSharedSpanSchema = z.object({
 export const getSharedSpan = async (input: z.infer<typeof GetSharedSpanSchema>) => {
   const { spanId, traceId } = GetSharedSpanSchema.parse(input);
 
-  const span = await db.query.spans.findFirst({
-    where: and(eq(spans.spanId, spanId), eq(spans.traceId, traceId)),
-  });
+  const [dbSpan, chResult] = await Promise.all([
+    db.query.spans.findFirst({
+      where: and(eq(spans.spanId, spanId), eq(spans.traceId, traceId)),
+      columns: {
+        spanId: true,
+        createdAt: true,
+        parentSpanId: true,
+        name: true,
+        attributes: true,
+        spanType: true,
+        startTime: true,
+        endTime: true,
+        traceId: true,
+        projectId: true,
+        inputUrl: true,
+        outputUrl: true,
+        status: true,
+      },
+    }),
+    clickhouseClient.query({
+      query: `
+        SELECT input, output
+        FROM spans
+        WHERE span_id = {spanId: UUID} AND trace_id = {traceId: UUID}
+        LIMIT 1
+      `,
+      format: "JSONEachRow",
+      query_params: { spanId, traceId },
+    }),
+  ]);
 
-  if (!span) {
-    throw new Error("Span not found or does not belong to the given trace");
+  if (!dbSpan) {
+    throw new Error("Span not found");
   }
 
-  return span;
+  const chData = (await chResult.json()) as [{ input: string; output: string }];
+  const { input: spanInput, output: spanOutput } = chData[0] || {};
+
+  return {
+    ...dbSpan,
+    input: tryParseJson(spanInput),
+    output: tryParseJson(spanOutput),
+  };
 };
 
 export const getSharedSpanEvents = async (input: z.infer<typeof GetSharedSpanSchema>) => {
@@ -44,4 +79,20 @@ export const getSharedSpanEvents = async (input: z.infer<typeof GetSharedSpanSch
   });
 
   return rows;
+};
+
+const tryParseJson = (value: string) => {
+  if (value === "") return null;
+
+  try {
+    return JSON.parse(value);
+  } catch (e) {
+    // Parse with brackets because we stringify array using comma separator on server.
+    try {
+      return JSON.parse(`[${value}]`);
+    } catch (e2) {
+      console.log("Failed to parse JSON with brackets:", e2);
+      return value;
+    }
+  }
 };
