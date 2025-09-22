a5edd1ff01ba161675af1164088e6edde368aaa3
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-09-22 16:55:45 +0100

    feat: add button

--- frontend/components/ui/code-highlighter/code-sheet.tsx
+++ frontend/components/ui/code-highlighter/code-sheet.tsx
@@ -62,7 +62,9 @@ const PureCodeSheet = ({ mode, renderedValue, extensions, onModeChange, placehol
             <div className="flex items-center">
               <CopyButton iconClassName="h-3.5 w-3.5" size="icon" variant="ghost" text={renderedValue} />
               <SheetClose asChild>
-                <Minimize className="h-4 w-4" />
+                <Button variant="ghost" size="icon">
+                  <Minimize className="h-4 w-4" />
+                </Button>
               </SheetClose>
             </div>
           </div>
