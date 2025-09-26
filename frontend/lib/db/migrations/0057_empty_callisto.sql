a934f0e2a22533b48de8ff257b63391801d3502b
Robert Kim <skull8888888@gmail.com>
2025-09-26 13:27:43 +0100

    Update frontend/lib/db/migrations/0057_empty_callisto.sql
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- frontend/lib/db/migrations/0057_empty_callisto.sql
+++ frontend/lib/db/migrations/0057_empty_callisto.sql
@@ -2,6 +2,6 @@ CREATE TABLE "project_settings" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"name" text,
 	"value" text,
-	"project_id" uuid DEFAULT gen_random_uuid() NOT NULL
+	"project_id" uuid NOT NULL
 );
 ALTER TABLE "project_settings" ADD CONSTRAINT "project_settings_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "public"."projects"("id") ON DELETE cascade ON UPDATE cascade;
