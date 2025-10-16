39f9f6396a99741aec7406e73e6d6b28935db891
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-10-16 14:31:05 +0100

    feat: remove vercel cron option

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -102,12 +102,6 @@ if (process.env.ENVIRONMENT === "PRODUCTION" && process.env.SENTRY_DSN) {
 
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
   });
 } else {
   module.exports = nextConfig;
