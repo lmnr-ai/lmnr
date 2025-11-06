dd533f5b52806a335cb04246c6ee8accca91884f
Din <dinmukhamed.mailibay@gmail.com>
2025-11-06 16:27:04 +0000

    fix migration

--- frontend/lib/db/migrations/0061_zippy_the_hood.sql
+++ frontend/lib/db/migrations/0061_zippy_the_hood.sql
@@ -1,6 +1,6 @@
 ALTER TABLE "tag_classes" DROP CONSTRAINT "tag_classes_name_project_id_unique";--> statement-breakpoint
 ALTER TABLE "event_definitions" DROP CONSTRAINT "event_definitions_project_id_fkey";
-> statement-breakpoint
+--> statement-breakpoint
 ALTER TABLE "project_api_keys" ADD COLUMN "is_ingest_only" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "tag_classes" DROP COLUMN "evaluator_runnable_graph";--> statement-breakpoint
 ALTER TABLE "tag_classes" DROP COLUMN "pipeline_version_id";--> statement-breakpoint
