833501551f0a97e474bef667e007c9c73127bd25
Din <dinmukhamed.mailibay@gmail.com>
2025-11-13 18:25:50 +0000

    allow SSL config for drizzle-kit, fix migration 61

--- frontend/drizzle.config.ts
+++ frontend/drizzle.config.ts
@@ -1,18 +1,33 @@
 import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig, type Config } from 'drizzle-kit';
+import { getDatabaseConfig } from './lib/db/drizzle';
 
 config({ path: '.env.local' });
 
+const dbConfig = getDatabaseConfig();
+const dbCredentials: Record<string, string | number | Record<string, any>> = {
+  user: dbConfig.username!,
+  password: dbConfig.password,
+  host: dbConfig.host!,
+  port: dbConfig.port,
+  database: dbConfig.database,
+};
+
+if (process.env.DATABASE_SSL_ROOT_CERT) {
+  dbCredentials.ssl = {
+    ca: process.env.DATABASE_SSL_ROOT_CERT,
+  };
+}
+
+
 export default defineConfig({
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
-  schema: './lib/db/migrations/schema.ts',
-  out: './lib/db/migrations',
+  dialect: "postgresql",
+  dbCredentials,
+  schema: "./lib/db/migrations/schema.ts",
+  out: "./lib/db/migrations",
   entities: {
     roles: {
-      provider: 'supabase',
-    }
-  }
-});
+      provider: "supabase",
+    },
+  },
+} as Config);
