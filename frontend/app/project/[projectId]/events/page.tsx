9913c539c015452408a68f20be8df01e67c9039c
Robert Kim <skull8888888@gmail.com>
2025-12-16 13:38:58 +0000

    feature flag

--- frontend/app/project/[projectId]/events/page.tsx
+++ frontend/app/project/[projectId]/events/page.tsx
@@ -4,7 +4,6 @@ import { redirect } from "next/navigation";
 export default async function EventsPage(props: { params: Promise<{ projectId: string }> }) {
   const { projectId } = await props.params;
 
-
   if (isFeatureEnabled(Feature.SEMANTIC_EVENTS)) {
     redirect(`/project/${projectId}/events/semantic`);
   } else {
