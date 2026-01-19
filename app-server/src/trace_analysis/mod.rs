dc2ffd87da2ed661c44479f75c2692dfa728f59f
Rakhman Asmatullayev <rakhmandva@gmail.com>
2026-01-19 17:34:31 +0000

    update message structure

--- app-server/src/trace_analysis/mod.rs
+++ app-server/src/trace_analysis/mod.rs
@@ -31,24 +31,31 @@ pub const TRACE_ANALYSIS_LLM_BATCH_PENDING_ROUTING_KEY: &str =
 pub struct RabbitMqLLMBatchSubmissionMessage {
     pub project_id: Uuid,
     pub job_id: Uuid,
-    pub payloads: Vec<Payload>,
+    pub event_definition_id: Uuid,
+    pub prompt: String,
+    pub structured_output_schema: Value,
+    pub model: String,
+    pub provider: String,
+    pub tasks: Vec<Task>,
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct RabbitMqLLMBatchPendingMessage {
     pub project_id: Uuid,
     pub job_id: Uuid,
-    pub batch_id: Uuid, // LLM Request Batch ID that can be used to track the completion of the batch
-    pub payloads: Vec<Payload>,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Payload {
-    pub task_id: Uuid,
-    pub event_defintion_id: Uuid,
+    pub event_definition_id: Uuid,
+    pub prompt: String,
     pub structured_output_schema: Value,
     pub model: String,
     pub provider: String,
+    pub tasks: Vec<Task>,
+    pub batch_id: Uuid, // LLM Request Batch ID that can be used to track the completion of the batch
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Task {
+    pub task_id: Uuid,
+    pub trace_id: Uuid,
 }
 
 async fn push_to_pending_queue(
