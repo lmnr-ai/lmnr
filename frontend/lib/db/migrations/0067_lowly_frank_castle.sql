51298f4c7e6132594df254d6795122c7898937d5
Olzhas Nurpeisov <olzhik1123@gmail.com>
2026-01-26 16:29:59 +0000

    feat: migrations of signals, jobs, triggers

--- frontend/lib/db/migrations/0067_lowly_frank_castle.sql
+++ frontend/lib/db/migrations/0067_lowly_frank_castle.sql
@@ -1,1 +1,29 @@
+CREATE TABLE "signal_jobs" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"signal_id" uuid NOT NULL,
+	"project_id" uuid NOT NULL,
+	"total_traces" integer DEFAULT 0 NOT NULL,
+	"processed_traces" integer DEFAULT 0 NOT NULL,
+	"failed_traces" integer DEFAULT 0 NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	"updated_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "signal_triggers" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"project_id" uuid NOT NULL,
+	"value" jsonb NOT NULL,
+	"signal_id" uuid NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "signals" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"project_id" uuid NOT NULL,
+	"name" text NOT NULL,
+	"prompt" text NOT NULL,
+	"structured_output_schema" jsonb NOT NULL,
+	"created_at" timestamp with time zone DEFAULT now() NOT NULL,
+	CONSTRAINT "signals_project_id_name_key" UNIQUE("project_id","name")
+);
 ALTER TABLE "traces" ADD COLUMN "span_names" jsonb;--> statement-breakpoint
