a85e441893d1234b27ac51da8e7e4e28f8880331
Olzhas Nurpeisov <74583784+olzhik11@users.noreply.github.com>
2026-02-19 18:26:03 +0000

    feat: add signal limits, small refactor (#1248)
    * feat: add signal limits, small refactor
    
    * feat: fix comments

--- frontend/components/workspace/workspace.tsx
+++ frontend/components/workspace/workspace.tsx
@@ -3,7 +3,7 @@
 import Projects from "@/components/projects/projects.tsx";
 import { useWorkspaceMenuContext } from "@/components/workspace/workspace-menu-provider.tsx";
 import { type SubscriptionDetails, type UpcomingInvoiceInfo } from "@/lib/actions/checkout/types";
-import { type WorkspaceStats } from "@/lib/usage/types";
+import { type WorkspaceStats } from "@/lib/actions/usage/types";
 import { type WorkspaceInvitation, type WorkspaceRole, type WorkspaceWithOptionalUsers } from "@/lib/workspaces/types";
 
 import WorkspaceBilling from "./billing";
