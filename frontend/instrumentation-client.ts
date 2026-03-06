1c58a9c03fcf717a593e86160d46dc5706ad864b
Olzhas Nurpeisov <olzhik1123@gmail.com>
2026-03-06 18:56:49 +0000

    feat: remove comment

--- frontend/instrumentation-client.ts
+++ frontend/instrumentation-client.ts
@@ -5,31 +5,31 @@
 import * as Sentry from "@sentry/nextjs";
 
 if (process.env.LAMINAR_CLOUD === "true") {
-  // Sentry.init({
-  //   dsn: "https://0acd62b621df6df93ef143408334027c@o4510193435475968.ingest.us.sentry.io/4510193666555904",
-  //
-  //   tracesSampleRate: 1,
-  //
-  //   integrations: [Sentry.browserTracingIntegration()],
-  //   beforeSendTransaction(event) {
-  //     if (
-  //       event.transaction &&
-  //       event.transaction.includes("/api/projects/") &&
-  //       event.transaction.includes("/realtime")
-  //     ) {
-  //       return null;
-  //     }
-  //
-  //     if (
-  //       (event.contexts?.trace?.op === "navigation" || event.contexts?.trace?.op === "pageload") &&
-  //       typeof window !== "undefined"
-  //     ) {
-  //       event.transaction = window.location.pathname;
-  //     }
-  //
-  //     return event;
-  //   },
-  // });
+  Sentry.init({
+    dsn: "https://0acd62b621df6df93ef143408334027c@o4510193435475968.ingest.us.sentry.io/4510193666555904",
+
+    tracesSampleRate: 1,
+
+    integrations: [Sentry.browserTracingIntegration()],
+    beforeSendTransaction(event) {
+      if (
+        event.transaction &&
+        event.transaction.includes("/api/projects/") &&
+        event.transaction.includes("/realtime")
+      ) {
+        return null;
+      }
+
+      if (
+        (event.contexts?.trace?.op === "navigation" || event.contexts?.trace?.op === "pageload") &&
+        typeof window !== "undefined"
+      ) {
+        event.transaction = window.location.pathname;
+      }
+
+      return event;
+    },
+  });
 }
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
