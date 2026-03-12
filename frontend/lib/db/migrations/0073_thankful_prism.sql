f94e158822073105b93d51b6acc43534eee7d0e8
Robert Kim <skull8888888@gmail.com>
2026-03-12 11:17:48 +0000

    clean

--- frontend/lib/db/migrations/0073_thankful_prism.sql
+++ frontend/lib/db/migrations/0073_thankful_prism.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "traces" ADD COLUMN "root_span_input" text;--> statement-breakpoint
-ALTER TABLE "traces" ADD COLUMN "root_span_output" text;--> statement-breakpoint
+ALTER TABLE "traces" ADD COLUMN IF NOT EXISTS "root_span_input" text;--> statement-breakpoint
+ALTER TABLE "traces" ADD COLUMN IF NOT EXISTS "root_span_output" text;--> statement-breakpoint
