ec8b4057799bde09684545c3f68ca462948b2ad6
Din <dinmukhamed.mailibay@gmail.com>
2026-03-22 10:49:19 +0000

    drop index from evaluation_datapoints_ordering key

---
+++ frontend/lib/clickhouse/migrations/32_evaluation_datapoints_order_key.sql
@@ -0,0 +1,26 @@
+CREATE TABLE new_evaluation_datapoints
+(
+    `id` UUID,
+    `evaluation_id` UUID,
+    `project_id` UUID,
+    `trace_id` UUID,
+    `updated_at` DateTime64(9, 'UTC'),
+    `data` String CODEC(ZSTD(3)),
+    `target` String CODEC(ZSTD(3)),
+    `metadata` String CODEC(ZSTD(3)),
+    `executor_output` String CODEC(ZSTD(3)),
+    `index` UInt64,
+    `dataset_id` UUID,
+    `dataset_datapoint_id` UUID,
+    `dataset_datapoint_created_at` DateTime64(9, 'UTC'),
+    `group_id` String,
+    `scores` String,
+    INDEX idx_project_eval_id_datapoint_id (project_id, evaluation_id, id) TYPE bloom_filter GRANULARITY 1
+)
+ENGINE = ReplacingMergeTree(updated_at)
+ORDER BY (project_id, evaluation_id, id);
+
+INSERT INTO new_evaluation_datapoints SELECT * FROM evaluation_datapoints;
+
+RENAME TABLE evaluation_datapoints TO old_evaluation_datapoints;
+RENAME TABLE new_evaluation_datapoints TO evaluation_datapoints;
