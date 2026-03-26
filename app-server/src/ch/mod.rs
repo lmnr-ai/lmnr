86d741729827a93c9e109d6b757d0fc594303e71
Din <dinmukhamed.mailibay@gmail.com>
2026-03-26 16:36:45 +0000

    disable writing to spans v2
    The table was migrated back to spans and is to be dropped

--- app-server/src/ch/mod.rs
+++ app-server/src/ch/mod.rs
@@ -26,7 +26,6 @@ use crate::db::workspaces::WorkspaceDeployment;
 #[derive(Serialize, Clone, Copy, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum Table {
-    SpansV2,
     Spans,
     Traces,
     NotificationLogs,
@@ -35,7 +34,6 @@ pub enum Table {
 impl Table {
     pub const fn as_str(&self) -> &'static str {
         match self {
-            Table::SpansV2 => "spans_v2",
             Table::Spans => "spans",
             Table::Traces => "traces_replacing",
             Table::NotificationLogs => "notification_logs",
